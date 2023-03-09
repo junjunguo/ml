ef3523911230e95e6a21cf88fad2636e191b8e51
GuoJunjun <guojunjun@gmail.com>
2023-03-09 15:42:52 +0100

    remove tfjs-vis, use chart js (#5)

--- webapp/packages/emotion/src/tfVisBarChart.tsx
+++ webapp/packages/emotion/src/tfVisBarChart.tsx
@@ -1,30 +1,35 @@
-import { render } from "@tensorflow/tfjs-vis";
-import React, { type FC, useEffect, useRef } from "react";
+// import { render } from "@tensorflow/tfjs-vis";
+// import React, { type FC, useEffect, useRef } from "react";
 
-import { emotionLabels } from "./constants";
+// import { emotionLabels } from "./constants";
 
-export const TfVisBarChart: FC<{
-  data: Array<{
-    index: number;
-    value: number;
-  }>;
-}> = ({ data }) => {
-  const ref = useRef<HTMLDivElement | null>(null);
+// /**
+//  * TODO: wait to new vision wich can fix :
+//  * npm WARN deprecated @types/fast-json-stable-stringify@2.1.0: This is a stub types definition. fast-json-stable-stringify provides its own type definitions, so you do not need this installed.
+// npm WARN deprecated core-js@1.2.7: core-js@<3.23.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Some versions have web compatibility issues. Please, upgrade your dependencies to the actual version of core-js.
+//  */
+// export const TfVisBarChart: FC<{
+//   data: Array<{
+//     index: number;
+//     value: number;
+//   }>;
+// }> = ({ data }) => {
+//   const ref = useRef<HTMLDivElement | null>(null);
 
-  const renderBarChart = async (): Promise<void> => {
-    const container = ref.current;
-    if (container != null) {
-      await render.barchart(container, data, {
-        xLabel: emotionLabels.map((v, i) => `${i}:${v}`).join(" "),
-        yLabel: "Posibility",
-      });
-    }
-  };
+//   const renderBarChart = async (): Promise<void> => {
+//     const container = ref.current;
+//     if (container != null) {
+//       await render.barchart(container, data, {
+//         xLabel: emotionLabels.map((v, i) => `${i}:${v}`).join(" "),
+//         yLabel: "Posibility",
+//       });
+//     }
+//   };
 
-  useEffect(() => {
-    if (ref.current != null && data?.length > 0)
-      renderBarChart().catch((e) => {});
-  }, [ref.current, data]);
+//   useEffect(() => {
+//     if (ref.current != null && data?.length > 0)
+//       renderBarChart().catch((e) => {});
+//   }, [ref.current, data]);
 
-  return <div ref={ref}></div>;
-};
+//   return <div ref={ref}></div>;
+// };
